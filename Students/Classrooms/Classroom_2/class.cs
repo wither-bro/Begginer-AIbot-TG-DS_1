d006a2b0459f862073dfc0893c1c3013562ca42e
wither-bro <alexpoloian@gmail.com>
2026-05-15 20:31:38 +0400

    Create class.cs

---
+++ Students/Classrooms/Classroom_2/class.cs
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* === REGISTERED STUDENTS LIST (Max 31) ===
+1. [Ник](https://t.me/username) (@username)
+2. ... место для новых учеников ...
+
+------------------------------------------- */
+
+// СИСТЕМНАЯ ШАПКА SERIES(CG)
+using System;
+
+namespace SeriesCG.Academy {
+    class Core {
+        static void Status() {
+            Console.WriteLine("Classroom logic initialized at the bottom.");
+        }
+    }
+}
